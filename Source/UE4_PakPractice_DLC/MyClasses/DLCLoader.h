d0cf14f201eaa276da7095dd0eb97d8101b26ea8
Николай <nikolai0302@yandex.ru>
2023-03-05 18:34:22 +0300

    создал 3 класса для работы
    также инициализировал райдер и настроил открытие карты Map1 в игре и в редакторе

---
+++ Source/UE4_PakPractice_DLC/MyClasses/DLCLoader.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "DLCLoader.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_PAKPRACTICE_DLC_API UDLCLoader : public UObject
+{
+	GENERATED_BODY()
+};
